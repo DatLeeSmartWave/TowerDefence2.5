6db48a14239dcb7436d36908a53417d497892c2d
DatLee <ledat@smartwave247.com>
2024-07-30 16:57:47 +0700

    +

--- Assets/Scripts/Player/SpawnIcon.cs
+++ Assets/Scripts/Player/SpawnIcon.cs
@@ -5,39 +5,62 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SpawnIcon : MonoBehaviour {
-  public IconTowerData[] iconTowerData;
-  public Button[] btnIcon;
-  private int priceSpawn = 100;
-  private void Start() {
-    LoadIconTower();
-  }
+    public IconTowerData[] iconTowerData;
+    public Button[] btnIcon;
+    int priceSpawn;
 
-  public void CreateIcon(GameObject iconSpawn) {
-    if (BuyIcon()) {
-      Vector3 pos = new Vector3(0, 0, 0);
-      GameObject icon = Instantiate(iconSpawn, pos, Quaternion.identity);
-      icon.SetActive(false);
-      icon.SetActive(true);
+    private void Start() {
+        LoadIconTower();
     }
-  }
 
-  public void LoadIconTower() {
-    for (int i = 0; i < iconTowerData.Length; i++) {
-      int index = i;
-      btnIcon[i].onClick.AddListener(() => CreateIcon(iconTowerData[index].prefabTower));
-      btnIcon[i].image.sprite = iconTowerData[i].imgIcon;
+    public void CreateIcon(GameObject iconSpawn) {
+        if (BuyIcon()) {
+            Vector3 pos = new Vector3(0, 0, 0);
+            GameObject icon = Instantiate(iconSpawn, pos, Quaternion.identity);
+            icon.SetActive(false);
+            icon.SetActive(true);
+        }
     }
-  }
 
-  public bool BuyIcon() {
-    if (RfHolder.Ins.coinPlayer.coinPlayer >= priceSpawn) {
-      RfHolder.Ins.coinPlayer.coinPlayer -= priceSpawn;
-      RfHolder.Ins.coinPlayer.UpdateTextCoin();
-      return true;
+    public void LoadIconTower() {
+        for (int i = 0; i < iconTowerData.Length; i++) {
+            int index = i;
+            btnIcon[i].onClick.AddListener(() => {
+                SetPriceSpawn(index);
+                CreateIcon(iconTowerData[index].prefabTower);
+            });
+            btnIcon[i].image.sprite = iconTowerData[i].imgIcon;
+        }
     }
-    else {
-      Debug.Log("Not enough coin");
-      return false;
+
+    public void SetPriceSpawn(int buttonIndex) {
+        switch (buttonIndex) {
+            case 0:
+                priceSpawn = 100;
+                break;
+            case 1:
+                priceSpawn = 200;
+                break;
+            case 2:
+                priceSpawn = 300;
+                break;
+            case 3:
+                priceSpawn = 400;
+                break;
+            default:
+                priceSpawn = 0;
+                break;
+        }
     }
-  }
+
+    public bool BuyIcon() {
+        if (RfHolder.Ins.coinPlayer.coinPlayer >= priceSpawn) {
+            RfHolder.Ins.coinPlayer.coinPlayer -= priceSpawn;
+            RfHolder.Ins.coinPlayer.UpdateTextCoin();
+            return true;
+        } else {
+            Debug.Log("Not enough coin");
+            return false;
+        }
+    }
 }
